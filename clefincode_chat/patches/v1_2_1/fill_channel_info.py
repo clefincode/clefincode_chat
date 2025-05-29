61cca5a6be7fdb5e26271c848463b398e3fd4705
Ahmad Kamal Eddin <ahmad@clefincode.com>
2025-05-29 10:55:13 +0000

    Fix: avoid Empty Channel Name

--- clefincode_chat/patches/v1_2_1/fill_channel_info.py
+++ clefincode_chat/patches/v1_2_1/fill_channel_info.py
@@ -17,7 +17,6 @@ def execute():
 
     for row in frappe.get_all(
             "ClefinCode Chat Channel",
-            filters={"channel_info": ""},
             fields=["name"]
         ):
         doc = frappe.get_doc("ClefinCode Chat Channel", row.name)
