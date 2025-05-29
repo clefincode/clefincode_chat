4a4d0a64c715a78db728faa4b529332f27c974dc
Ahmad Kamal Eddin <ahmad@clefincode.com>
2025-05-29 10:31:50 +0000

    Fix: Handle new field in channel search during old data migration

--- clefincode_chat/patches/v1_2_1/fill_channel_info.py
+++ clefincode_chat/patches/v1_2_1/fill_channel_info.py
@@ -2,6 +2,19 @@ import frappe
 
 def execute():
 
+
+    from frappe.custom.doctype.custom_field.custom_field import create_custom_field
+    # Ensure 'channel_info' field on ClefinCode Chat Channel
+    if not frappe.db.has_column("ClefinCode Chat Channel", "channel_info"):
+        create_custom_field("ClefinCode Chat Channel", {
+            "fieldname": "channel_info",
+            "label": "Channel Info",
+            "fieldtype": "Long Text",
+        })
+
+        frappe.reload_doctype("ClefinCode Chat Channel")
+
+
     for row in frappe.get_all(
             "ClefinCode Chat Channel",
             filters={"channel_info": ""},
