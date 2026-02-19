5ebe8f67083a635bda86886f0d35a334c3bb7dbc
ALI_93915 <ali.a@clefincode.com>
2026-02-19 13:46:29 +0000

    chore: remove installation requirements from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+
 
 
 setup(
@@ -12,6 +11,5 @@ setup(
 	author_email="info@clefincode.com",
 	packages=find_packages(),
 	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+	include_package_data=True
 )
