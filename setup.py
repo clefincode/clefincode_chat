c2deb7e826ffbae16b40889a3097a10294dac048
ALI_93915 <ali.a@clefincode.com>
2026-02-19 12:33:39 +0000

    Fix version assignment in setup.py to use hardcoded version '1.3.3'

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in clefincode_chat/__init__.py
-from clefincode_chat import __version__ as version
 
 setup(
 	name="clefincode_chat",
-	version=version,
+	version='1.3.3',
 	description="ERPNext & Frappe Business Chat: A self-hosted communication solution.",
 	author="ClefinCode L.L.C-FZ",
 	author_email="info@clefincode.com",
